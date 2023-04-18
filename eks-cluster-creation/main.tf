fc0933c294a2527685d3ded40cb80601f745406b
unknown <yogisdmec007@gmail.com>
2023-04-18 11:57:54 +0530

    code update

--- eks-cluster-creation/main.tf
+++ eks-cluster-creation/main.tf
@@ -32,7 +32,7 @@ resource "aws_subnet" "public-subnets" {
 data "aws_route_table" "publicrt" {
   filter {
    name = "tag:Name"
-   values = "Default-Route-Table"
+   values = "Default-Route-*"
   }
 }
 
